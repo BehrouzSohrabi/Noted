e6611169476d562e15736aa665b7d2793a4926f6
Behrouz Sohrabi <sohrabi.beh@gmail.com>
2022-12-08 23:24:03 -0700

    added react & editor js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useMemo, useState, Suspense } from 'react';
+import Editor from './Editor';
+import './App.css';
+
+const App = () => {
+  return (
+    <Editor />
+  );
+};
+
+export default App;
